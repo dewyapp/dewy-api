e0cad4cf45de515bddf09802d22d0b1b3ba1cec5
King Chung Huang <kinghuang@mac.com>
2015-11-01 16:57:31 -0700

    Add routes for modules under sites
    This route will return the modules for a given site.

--- sites/__init__.py
+++ sites/__init__.py
@@ -4,3 +4,5 @@ from . import base
 sites = Blueprint('sites', __name__)
 sites.add_url_rule('/', 'sites', base.sites)
 sites.add_url_rule('/<site>', 'site', base.site)
+sites.add_url_rule('/<site>/modules', 'modules', modules.modules)
+sites.add_url_rule('/<site>/modules/<module>', 'module', modules.module)
---
+++ sites/modules.py
@@ -0,0 +1,5 @@
+def modules(site):
+	return 'Modules in the current context.'
+
+def module(site, module):
+	return 'Module %s in the current context.' % module
