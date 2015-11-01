ac5f198cbc02ba38f78d4ffab5a13c909edba99b
King Chung Huang <kinghuang@mac.com>
2015-11-01 16:56:40 -0700

    Register blueprints for sites, modules, and nodes
    Add the blueprints for sites, modules, and nodes to the base
    application.

--- api.py
+++ api.py
@@ -2,9 +2,17 @@
 
 from flask import Flask
 
+from sites import sites
+from modules import modules
+from nodes import nodes
+
 
 def main():
 	app = Flask(__name__)
+
+	app.register_blueprint(sites, url_prefix='/sites')
+	app.register_blueprint(modules, url_prefix='/modules')
+	app.register_blueprint(nodes, url_prefix='/nodes')
 
 	app.run(
 		host='0.0.0.0',
