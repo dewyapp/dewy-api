ffe0897c6dc297de23b4373478443010cc9ad939
Derek McBurney <d.mcburney@icloud.com>
2016-01-18 21:49:24 -0700

    Enables API access from dewy.io

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Global declaration of Couchbase
 module.exports.bucket = (new couchbase.Cluster(config.couchbase.server)).openBucket(config.couchbase.bucket);
 
+// Allow API access from dewy.io
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://dewy.io');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
+
 // API endpoints
 var routes = require('./routes/api');
 app.use('/', routes);
