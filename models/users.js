561b0be546cceead67e0ac36ff0f74392255f273
Derek McBurney <d.mcburney@icloud.com>
2016-01-19 22:29:50 -0700

    Adds user creation
    The Dewy web app will call this endpoint. There seems to be a bug where
    checking if the email address is in use gives the result from a
    previous state.

---
+++ models/users.js
@@ -0,0 +1,34 @@
+var uuid = require('uuid');
+var forge = require("node-forge");
+var couchbase = require('couchbase');
+var db = require('../app.js').bucket;
+
+exports.create = function(user, callback) {
+    // Construct user document
+    var userDoc = {
+        uid: uuid.v4(),
+        apikey: uuid.v4(),
+        username: user.username,
+        email: user.email,
+        password: forge.md.sha1.create().update(user.password).digest().toHex()
+    };
+    db.insert('user::' + userDoc.uid, userDoc, function(error, result) {
+        if (error) {
+            callback(error, null);
+            return;
+        }
+        callback(null, {message: 'success', data: result});
+    });
+}
+
+exports.getByEmail = function(email, callback) {
+    query = couchbase.ViewQuery.from('dev_users', 'by_email')
+        .key([email]);
+    db.query(query, function(error, result) {
+        if (error) {
+            callback(error, null);
+            return;
+        }
+        callback(null, {message: 'success', data: result});
+    });
+}
--- routes/api.js
+++ routes/api.js
@@ -1,75 +1,101 @@
 var express = require('express');
 var router = express.Router();
 var util = require('util');
+var validator = require('validator');
 var filters = require('../models/filters');
 var sites = require('../models/sites');
 var tags = require('../models/tags');
+var users = require('../models/users');
 
 router.get('/fields', function (req, res, next) {
-  res.send(filters.getFields());
+    res.send(filters.getFields());
 });
 
 router.delete('/filters/:filter', function (req, res, next) {
-  console.log(util.inspect(req.body, {showHidden: false, depth: null}));
-  // res.send(filters.delete(req.params.filter));
+    console.log(util.inspect(req.body, {showHidden: false, depth: null}));
+    // res.send(filters.delete(req.params.filter));
 });
 
 router.get('/filters', function (req, res, next) {
-  res.send(filters.getAll(null));
+    res.send(filters.getAll(null));
 });
 
 router.get('/filters/:filter', function (req, res, next) {
-  res.send(filters.get(null, req.params.filter));
+    res.send(filters.get(null, req.params.filter));
 });
 
 router.post('/filters', function (req, res, next) {
-  console.log(util.inspect(req.body, {showHidden: false, depth: null}));
-  res.send();
+    console.log(util.inspect(req.body, {showHidden: false, depth: null}));
+    res.send();
 });
 
 router.put('/filters/:filter', function (req, res, next) {
-  console.log(util.inspect(req.body, {showHidden: false, depth: null}));
-  res.send();
+    console.log(util.inspect(req.body, {showHidden: false, depth: null}));
+    res.send();
 });
 
 router.get('/operators', function (req, res, next) {
-  res.send(filters.getOperators());
+    res.send(filters.getOperators());
 });
 
 router.post('/sites', function (req, res, next) {
-  sites.create(req.body, function(error, result) {
-    if (error) {
-      return res.status(400).send(error);
-    }
-    res.send(result);
-  });
+    sites.create(req.body, function(error, result) {
+        if (error) {
+            return res.status(400).send(error);
+        }
+        res.send(result);
+    });
 });
 
 router.put('/sites', function (req, res, next) {
-  console.log(util.inspect(req.body, {showHidden: false, depth: null}));
-  // res.send(sites.audit(req.body));
+    console.log(util.inspect(req.body, {showHidden: false, depth: null}));
+    // res.send(sites.audit(req.body));
 });
 
 router.get('/sites/:site?', function (req, res, next) {
-  res.send(sites.get(null, req.params.site));
+    res.send(sites.get(null, req.params.site));
 });
 
 router.put('/sites/:site?', function (req, res, next) {
-  console.log(util.inspect(req.body, {showHidden: false, depth: null}));
-  // res.send(sites.update(null, req.params.site));
+    console.log(util.inspect(req.body, {showHidden: false, depth: null}));
+    // res.send(sites.update(null, req.params.site));
 });
 
 router.get('/sites/_filter/:filter?', function (req, res, next) {
-  res.send(sites.getAll(null, req.params.filter));
+    res.send(sites.getAll(null, req.params.filter));
 });
 
 router.get('/tags', function (req, res, next) {
-  res.send(tags.getAll(null));
+    res.send(tags.getAll(null));
 });
 
 router.post('/users', function (req, res, next) {
-  console.log(util.inspect(req.body, {showHidden: false, depth: null}));
-  // res.send(users.create(req.body));
+    if (!req.body.username) {
+        return res.send({"status": "error", "message": "A username is required"});
+    }
+    if (!req.body.email) {
+        return res.send({"status": "error", "message": "An email address is required"});
+    }
+    if (!validator.isEmail(req.body.email)) {
+        return res.send({"status": "error", "message": "A valid email address is required"});
+    }
+    if (!req.body.password) {
+        return res.send({"status": "error", "message": "A password is required"});
+    }
+    users.getByEmail(req.body.email, function(error, result) {
+        if (error) {
+            return res.status(400).send(error);
+        }
+        if (result.data.length) {
+            return res.send({"status": "error", "message": "This email address is in use"});
+        }
+        users.create(req.body, function(error, result) {
+            if (error) {
+                return res.status(400).send(error);
+            }
+            res.send(result);
+        });
+    });
 });
 
 module.exports = router;
