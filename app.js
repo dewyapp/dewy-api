65304680ae214314b60db2245e5bde87dc7f23e5
Derek McBurney <d.mcburney@icloud.com>
2016-01-24 23:23:28 -0700

    Adds Couchbase design document set up on npm install
    The design documents don’t seem to be getting added to Couchbase
    consistently. I’m not sure what’s not working yet.

--- app.js
+++ app.js
@@ -13,42 +13,56 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Global declaration of Couchbase
 module.exports.bucket = (new couchbase.Cluster(config.couchbase.server)).openBucket(config.couchbase.bucket, config.couchbase.password);
 
-// Allow API access from dewy.io
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://dewy.io');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+// If setup is specified, run it
+argv = process.argv.splice(2);
+if (argv[0] === '--setup') {
+    var design = require('./setup.js');
+    design.setup(function(error, result) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(result);
+        }
+        process.exit(0);
+    });
+}
+else {
+    // Allow API access from dewy.io
+    app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', 'http://dewy.io');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-    next();
-});
+        next();
+    });
 
-// OAuth 2 configuration
-app.oauth = oauthserver({
-    model: require('./models/oauth'),
-    grants: ['authorization_code', 'password', 'refresh_token'],
-    accessTokenLifetime: 1800,
-    refreshTokenLifetime: 1209600,
-    debug: false
-});
-module.exports.oauth = app.oauth;
-app.all('/oauth/token', app.oauth.grant());
+    // OAuth 2 configuration
+    app.oauth = oauthserver({
+        model: require('./models/oauth'),
+        grants: ['authorization_code', 'password', 'refresh_token'],
+        accessTokenLifetime: 1800,
+        refreshTokenLifetime: 1209600,
+        debug: false
+    });
+    module.exports.oauth = app.oauth;
+    app.all('/oauth/token', app.oauth.grant());
 
-// API endpoints
-var fieldRoutes = require('./routes/fields');
-var filterRoutes = require('./routes/filters');
-var siteRoutes = require('./routes/sites');
-var tagRoutes = require('./routes/tags');
-var userRoutes = require('./routes/users');
-app.use('/fields', fieldRoutes);
-app.use('/filters', filterRoutes);
-app.use('/sites', siteRoutes);
-app.use('/tags', tagRoutes);
-app.use('/users', userRoutes);
+    // API endpoints
+    var fieldRoutes = require('./routes/fields');
+    var filterRoutes = require('./routes/filters');
+    var siteRoutes = require('./routes/sites');
+    var tagRoutes = require('./routes/tags');
+    var userRoutes = require('./routes/users');
+    app.use('/fields', fieldRoutes);
+    app.use('/filters', filterRoutes);
+    app.use('/sites', siteRoutes);
+    app.use('/tags', tagRoutes);
+    app.use('/users', userRoutes);
 
-// Error handling
-app.use(app.oauth.errorHandler());
-app.use(function(req, res) {
-  res.status(403).send("Not a valid API endpoint");
-});
-
+    // Error handling
+    app.use(app.oauth.errorHandler());
+    app.use(function(req, res) {
+      res.status(403).send("Not a valid API endpoint");
+    });
+}
 module.exports = app;
---
+++ setup.js
@@ -0,0 +1,114 @@
+var couchbase = require('couchbase');
+var db = require('./app.js').bucket;
+
+exports.setup = function (callback) {
+    // Design documents
+    var design_docs = {
+        oauth: {
+            views: {
+                by_accesstoken: {
+                    map: [ 
+                        'function (doc, meta) {',
+                            'if (meta.id.substring(0, 13) == "accesstoken::") {',
+                                'emit([doc.access_token], {access_token: doc.access_token, client_id: doc.client_id, expires: doc.expires, userId: doc.uid});',
+                            '}',
+                        '}'
+                        ].join('\n')
+                },
+                by_clientid: {
+                    map: [
+                        'function (doc, meta) {',
+                            'if (meta.id.substring(0, 8) == "client::") {',
+                                'emit([doc.client_id], {client_id: doc.client_id, client_secret: doc.client_secret});',
+                            '}',
+                        '}'
+                        ].join('\n')
+                },
+                by_refreshtoken: {
+                    map: [
+                        'function (doc, meta) {',
+                            'if (meta.id.substring(0, 14) == "refreshtoken::") {',
+                                'emit([doc.refresh_token], {refresh_token: doc.refresh_token, client_id: doc.client_id, expires: doc.expires, userId: doc.uid});',
+                            '}',
+                        '}'
+                        ].join('\n')
+                }
+            }
+        },
+        sites: {
+            views: {
+                by_baseurl: {
+                    map: [
+                        'function (doc, meta) {',
+                            'if (meta.id.substring(0, 6) == "site::") {',
+                                'emit([doc.uid, doc.baseurl], doc.sid);',
+                            '}',
+                        '}'
+                        ].join('\n')
+                },
+                by_uid: {
+                    map: [
+                        'function (doc, meta) {',
+                            'if (meta.id.substring(0, 6) == "site::") {',
+                                'emit([doc.uid], doc.sid);',
+                            '}',
+                        '}'
+                        ].join('\n')
+                }
+            }
+        },
+        users: {
+            views: {
+                by_apikey: {
+                    map: [
+                        'function (doc, meta) {',
+                            'if (meta.id.substring(0, 6) == "user::") {',
+                                'emit([doc.apikey], doc.uid);',
+                            '}',
+                        '}'
+                        ].join('\n')
+                },
+                by_email: {
+                    map: [
+                        'function (doc, meta) {',
+                            'if (meta.id.substring(0, 6) == "user::") {',
+                                'emit([doc.email], doc.uid);',
+                            '}',
+                        '}'
+                        ].join('\n')
+                },
+                by_username: {
+                    map: [
+                        'function (doc, meta) {',
+                            'if (meta.id.substring(0, 6) == "user::") {',
+                                'emit([doc.username], doc.uid);',
+                            '}',
+                        '}'
+                        ].join('\n')
+                },
+                by_username_and_password: {
+                    map: [
+                        'function (doc, meta) {',
+                            'if (meta.id.substring(0, 6) == "user::") {',
+                                'emit([doc.username, doc.password], doc.uid);',
+                            '}',
+                        '}'
+                        ].join('\n')
+                }
+            }
+        }
+    }
+
+    // Insert or update design documents
+    var manager = db.manager();
+    for (design_doc in design_docs) {
+        manager.upsertDesignDocument(design_doc, design_docs[design_doc], function(error, result) {
+            if (error) {
+                callback(error, null);
+                return;
+            }
+        });
+    }
+
+    callback(null, {message: 'success'});
+}
