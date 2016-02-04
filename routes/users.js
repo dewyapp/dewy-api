51a41ffc028ff5991731a4288402c56c23af2f4b
Derek McBurney <d.mcburney@icloud.com>
2016-02-04 09:53:18 -0700

    Changes user creation error validation to give all errors
    Because of nested callbacks, I could only report an error at a time.
    With async, I can run through all validations and send back the all
    errors when they’re all processed.

--- routes/users.js
+++ routes/users.js
@@ -1,49 +1,78 @@
 var express = require('express');
 var router = express.Router();
 var oauth = require('../app.js').oauth;
+var async = require('async');
 var validator = require('validator');
 var users = require('../models/users');
 
 router.post('/', function (req, res, next) {
-    console.log(req.body);
-    if (!req.body.username) {
-        return res.send({"status": "error", "message": "A username is required."});
-    }
-    if (!req.body.email) {
-        return res.send({"status": "error", "message": "An email address is required."});
-    }
-    if (!validator.isEmail(req.body.email)) {
-        return res.send({"status": "error", "message": "A valid email address is required."});
-    }
-    if (!req.body.password) {
-        return res.send({"status": "error", "message": "A password is required."});
-    }
-    // Check if username exists
-    users.getByUsername(req.body.username, function(error, result) {
+    async.parallel({
+        username: function(callback) {
+            if (!req.body.username) {
+                callback(null, "A username is required.");
+                return;
+            } else {
+                // Check if username is in use
+                users.getByUsername(req.body.username, function(error, result) {
+                    if (error) {
+                        callback(error);
+                        return;
+                    }
+                    if (result.data.length) {
+                        callback(null, "This username is in use.");
+                        return;
+                    }
+                    callback();
+                });
+            }
+        },
+        email: function(callback) {
+            if (!req.body.email) {
+                callback(null, "An email address is required.");
+                return;
+            }
+            else if (!validator.isEmail(req.body.email)) {
+                callback(null, "A valid email address is required.");
+                return;
+            }
+            else {
+                // Check if email exists
+                users.getByEmail(req.body.email, function(error, result) {
+                    if (error) {
+                        callback(error);
+                        return;
+                    }
+                    if (result.data.length) {
+                        callback(null, "This email address is in use.");
+                        return;
+                    }
+                    callback();
+                });
+            }
+        },
+        password: function(callback) {
+            if (!req.body.password) {
+                callback(null, "A password is required.");
+                return
+            }
+            callback();
+        }
+    }, function(error, results) {
         if (error) {
             return res.status(400).send(error);
         }
-        if (result.data.length) {
-            return res.send({"status": "error", "message": "This username is in use."});
-        }
 
-        // Check if email exists
-        users.getByEmail(req.body.email, function(error, result) {
-            if (error) {
-                return res.status(400).send(error);
-            }
-            if (result.data.length) {
-                return res.send({"status": "error", "message": "This email address is in use."});
-            }
-
-            // User is unique, create them
+        // User validation passed, create the user
+        if (!results.username && !results.email && !results.password) {
             users.create(req.body, function(error, result) {
                 if (error) {
                     return res.status(400).send(error);
                 }
                 res.send(result);
             });
-        });
+        } else {
+            res.send(results);
+        }
     });
 });
 
