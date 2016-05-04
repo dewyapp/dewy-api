37c241046a5f1684b12395deb0db6c14053be17b
Derek McBurney <d.mcburney@icloud.com>
2016-05-04 00:02:58 -0600

    Fixes issue where no audited modules crashes API on module view

--- models/modules.js
+++ models/modules.js
@@ -79,38 +79,43 @@ exports.getAll = function(uid, fid, callback) {
         }
 
         // Get Drupal.org update information and determine updates
-        db.getMulti(projectKeys, function(error, result) {
-            for (module in modules) {
-                var updates = result['project::' + modules[module].package + '-' + modules[module].core];
-                modules[module].updates = 0;
-                modules[module].securityUpdates = 0;
-                for (version in modules[module].versions) {
-                    if (updates && 'value' in updates) {
-                        var securityUpdate = false;
-                        var update = false;
-                        for (release in updates.value.releases) {
-                            if (updates.value.releases[release].version == version) {
-                                if (securityUpdate) {
-                                    modules[module].securityUpdates = modules[module].securityUpdates + modules[module].versions[version].totalInstalls;
-                                    modules[module].updates = modules[module].updates + modules[module].versions[version].totalInstalls;
-                                } 
-                                else if (update) {
-                                    modules[module].updates = modules[module].updates + modules[module].versions[version].totalInstalls;
+        if (!projectKeys.length) {
+            callback(null, modules);
+        }
+        else {
+            db.getMulti(projectKeys, function(error, result) {
+                for (module in modules) {
+                    var updates = result['project::' + modules[module].package + '-' + modules[module].core];
+                    modules[module].updates = 0;
+                    modules[module].securityUpdates = 0;
+                    for (version in modules[module].versions) {
+                        if (updates && 'value' in updates) {
+                            var securityUpdate = false;
+                            var update = false;
+                            for (release in updates.value.releases) {
+                                if (updates.value.releases[release].version == version) {
+                                    if (securityUpdate) {
+                                        modules[module].securityUpdates = modules[module].securityUpdates + modules[module].versions[version].totalInstalls;
+                                        modules[module].updates = modules[module].updates + modules[module].versions[version].totalInstalls;
+                                    } 
+                                    else if (update) {
+                                        modules[module].updates = modules[module].updates + modules[module].versions[version].totalInstalls;
+                                    }
+                                    break;
                                 }
-                                break;
-                            }
-                            else if (updates.value.releases[release].securityUpdate) {
-                                securityUpdate = true;
-                            }
-                            else {
-                                update = true;
+                                else if (updates.value.releases[release].securityUpdate) {
+                                    securityUpdate = true;
+                                }
+                                else {
+                                    update = true;
+                                }
                             }
                         }
                     }
                 }
-            }
-            callback(null, modules);
-        });
+                callback(null, modules);
+            });
+        }
     });
 }
 
