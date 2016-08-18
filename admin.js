a989db0865bcaea9c43ab8485e58a6118be5ab2c
Derek McBurney <d.mcburney@icloud.com>
2016-08-18 01:13:11 -0600

    Adds random password generation for created users

--- admin.js
+++ admin.js
@@ -1,10 +1,11 @@
+var randomstring = require('randomstring');
 var User = require('./models/user');
 var email = require('./helpers/email');
 var config = new require('./config')();
 
 exports.createUser = function(emailAddress, username, callback) {
     var user = new User(emailAddress, username);
-    user.setPassword('RANDOMPASSWORDGOESHERE');
+    user.setPassword(randomstring.generate(8));
     user.removeVerification();
     user.create(function(error, result) {
         if (error) {
