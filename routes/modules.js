ce8867867660598f8d6bc2eac9c9d9643f2835e9
Derek McBurney <d.mcburney@icloud.com>
2016-09-08 23:40:07 -0600

    Adds non-filter sites end point that doesn't need subscription
    Modules end point also locked down with subscription

--- routes/modules.js
+++ routes/modules.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var oauth = require('../api.js').oauth;
+var subscription = require('../middleware/subscription.js');
 var filters = require('../models/filters');
 var modules = require('../models/modules');
 
-router.get('/_filter/:fid?', oauth.authorise(), function (req, res, next) {
+router.get('/_filter/:fid?', oauth.authorise(), subscription.require('basic'), function (req, res, next) {
     filters.get(req.params.fid, function(error, result) {
         if (error) {
             return res.status(500).send(error);
