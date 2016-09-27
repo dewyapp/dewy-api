f5bf9ef0343290f29eb1ca3e18f196a82244e0c8
Derek McBurney <d.mcburney@icloud.com>
2016-09-27 00:13:41 -0600

    Changes trial length to 14 days

--- models/user.js
+++ models/user.js
@@ -33,7 +33,7 @@ function User(email, username, password, gravatar, apikey, uid, verified, passwo
     this.created = created || Math.round(new Date().getTime() / 1000);
     this.subscription = {
         startDate: startDate || this.created,
-        endDate: endDate|| this.created + (60*60*24*10),
+        endDate: endDate|| this.created + (60*60*24*14),
         type: type || 'basic',
         stripeID: stripeID || false,
         subscriptionID: subscriptionID || false,
